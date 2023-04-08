136d23382a6e50dd3842c68e73eb56bdc937b75d
Mike Audi <mike@mytiki.com>
2023-04-08 00:30:35 -0500

    remove button (added in guide)

--- ios/swiftUI/swiftUI/ContentView.swift
+++ ios/swiftUI/swiftUI/ContentView.swift
@@ -12,19 +12,6 @@ struct ContentView: View {
         VStack {
             Image("iconLogo").imageScale(.large)
             Text("Let's do this shit").padding(.bottom, 24)
-            Button(action: {
-                do{
-                    //try? TikiSdk.present()
-                }catch{
-                    print(error)
-                }
-            }) {
-                Text("Show Offer")
-            }
-            .foregroundColor(Color(red: 1, green: 0.722, blue: 0.133))
-            .padding(12)
-            .background(Color(red: 0.11, green: 0, blue: 0))
-            .cornerRadius(8)
         }
         .padding()
     }
