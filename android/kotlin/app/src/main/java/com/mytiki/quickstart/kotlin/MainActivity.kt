2c17e06ca6f0be473116a3ba4bbf44e4cbd316cc
Mike Audi <mike@mytiki.com>
2023-04-12 23:30:06 -0500

    remove tiki for fresh start

--- android/kotlin/app/src/main/java/com/mytiki/quickstart/kotlin/MainActivity.kt
+++ android/kotlin/app/src/main/java/com/mytiki/quickstart/kotlin/MainActivity.kt
@@ -26,33 +26,8 @@ import java.util.*
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        TikiSdk
-            .offer
-            .ptr("test_offer")
-            .reward(ResourcesCompat.getDrawable(resources, R.drawable.reward, null)!!)
-            .bullet("Learn how our ads perform ", true)
-            .bullet("Reach you on other platforms", false)
-            .bullet("Sold to other companies", false)
-            .use(listOf(LicenseUsecase(LicenseUsecaseEnum.SUPPORT)))
-            .permission(Permission.CAMERA)
-            .tag(TitleTag.ADVERTISING_DATA)
-            .description("Trade your IDFA (kind of like a serial # for your phone) for a discount.")
-            .terms(this, "terms.md")
-            .duration(365, TimeUnit.DAYS)
-            .add()
-            .onAccept { _, _ -> print("accepted") }
-            .onDecline { _, _ -> print("declined") }
-            //.onSettings  { print("settings") }
-            .disableAcceptEnding(false)
-            .disableDeclineEnding(false)
-            .init(this, "e12f5b7b-6b48-4503-8b39-28e4995b5f88", UUID.randomUUID().toString()) {
-                setContentView(R.layout.activity_main)
-                findViewById<Button>(R.id.start).setOnClickListener {
-                    TikiSdk.present(this)
-                }
-            }
+        setContentView(R.layout.activity_main)
     }
 }
